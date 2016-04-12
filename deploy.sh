945da0d04c9e1d5084d70d2b23c386e3d579b258
lilinj2000 <lilinj2000@gmail.com>
2016-04-12 07:26:05 +0000

    updated.

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,9 @@
 
 ./autogen.sh
 
-./configure --prefix=/llj/3rd/libzmq sodium_CFLAGS="-I /llj/3rd/libsodium/include" sodium_LIBS=/llj/3rd/libsodium/lib/libsodium.la
+home_3rd=~/3rd
+
+./configure --prefix=$home_3rd/libzmq sodium_CFLAGS="-I $home_3rd/libsodium/include" sodium_LIBS=$home_3rd/libsodium/lib/libsodium.la
 
 make 
 
